1f25525182b3a5d191794e1b8ed6891e41f4d237
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-03-16 15:45:43 +0000

    test

--- testproject/settings.py
+++ testproject/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'testproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
