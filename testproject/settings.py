b330bbf3c42aee4c5695dd396b25031cb2c77761
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-03-16 15:02:59 +0000

    test

--- testproject/settings.py
+++ testproject/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     #'cloudinary_storage',
-    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     #'cloudinary',
     'blogs',
