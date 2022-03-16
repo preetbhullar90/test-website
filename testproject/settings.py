35aa8c32da1367b1bbc2b4c69e013f259c8c0c78
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-03-16 19:17:25 +0000

    test

--- testproject/settings.py
+++ testproject/settings.py
@@ -137,11 +137,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
- 
-STATIC_URL = '/static_cdn/'
+
+STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_cdn')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
