9da9ab3ac61d344cac63131fe354e60bef9bf2da
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-03-16 16:14:16 +0000

    test

--- testproject/urls.py
+++ testproject/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blogs/', include('blogs.urls', namespace='blogs')),
 
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
+] 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
