75c011e5e1c31da4f7f6bde6e006f21a42d06595
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-03-16 19:31:58 +0000

    test

--- testproject/urls.py
+++ testproject/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blogs/', include('blogs.urls', namespace='blogs')),
+    path('', include('blogs.urls', namespace='blogs')),
 
 ] 
 
